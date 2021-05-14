977a185877462dbab57cde444ea208de470f5ce7
zhangtao <zhangt12@mingyuanyun.com>
2021-05-14 15:27:05 +0800

    test: 测试提交的格式化

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const test = 'hello';
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+
+                <p>
+                    Edit <code>src/App.tsx</code>
+                    and save to reload. sdf
+                </p>
+
+                <div>我来加一行 试试它的格式化{test} world</div>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
